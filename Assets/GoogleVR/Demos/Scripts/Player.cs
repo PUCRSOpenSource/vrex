05fccf62bc74bbb81bf3169cc961be9d0ff17d14
Matthias Nunes <matthiasnunes@gmail.com>
2017-10-30 21:25:44 -0200

    [#00] Initial commit

---
+++ Assets/GoogleVR/Demos/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 3f;
+	public Transform cameraTransform;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float moveVertical = Input.GetAxis("Vertical");
+		Vector3 movement = cameraTransform.forward * moveVertical * speed * Time.deltaTime;
+		transform.Translate (movement);
+	}
+}
