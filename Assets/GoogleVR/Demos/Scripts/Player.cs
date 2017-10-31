65cb64ea740a6121b80bcc8296b75efeaac66858
Matthias Nunes <matthiasnunes@gmail.com>
2017-10-31 00:46:27 -0200

    [#00] Finishes player movement

--- Assets/GoogleVR/Demos/Scripts/Player.cs
+++ Assets/GoogleVR/Demos/Scripts/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	public float speed = 3f;
+	public float speed = 5f;
 	public Transform cameraTransform;
 
 	// Use this for initialization
@@ -15,7 +15,10 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		float moveVertical = Input.GetAxis("Vertical");
+		float moveHorizontal = Input.GetAxis("Horizontal");
 		Vector3 movement = cameraTransform.forward * moveVertical * speed * Time.deltaTime;
+		transform.Translate (movement);
+		movement = cameraTransform.right * moveHorizontal * speed * Time.deltaTime;
 		transform.Translate (movement);
 	}
 }
